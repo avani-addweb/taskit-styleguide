72a1509d648f51a6e272a79cfdd2bb0997288152
mini-addweb <mini.addweb@gmail.com>
2017-09-28 13:25:18 +0530

    Design of add task 1 page

--- docs/styleguide/assets/src/js/theme.js
+++ docs/styleguide/assets/src/js/theme.js
@@ -1,3 +1,7 @@
 jQuery(document).ready(function() {
-  // code...
+  // Custom input type file design 
+  jQuery('input[type="file"]').once().wrap('<div class="input-file"><div class="input-file-sub"></div></div>');
+  jQuery('.input-file').once().prepend('<span class="input-file-name">File name here</span>');
+
+  alert("I am an alert box!");
 });
